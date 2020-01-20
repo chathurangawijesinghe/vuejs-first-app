803a0ca8b037c27b8dbb72a996a29e17dd4dbc1a
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-01-20 18:33:16 +0800

    components

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+Vue.component('app-username', {
+  props: ['username'],
+  data: function() {
+    return {
+      // username: 'Test'
+    };
+  },
+  template: '<p>{{ username }}</p>'
+});
+
 new Vue({
 	el: '#app',
   data: {
-  	name: 'Max'
+    name: 'Max',
+    elements: []
   },
   methods: {
   	changeName: function() {
     	this.name = 'Anna';
+    },
+    addElement: function() {
+      this.elements.push(this.elements.length + 1);
+    },
+    getColor: function(number) {
+      return number % 2 == 0 ? 'green' : 'red';
     }
+  }
+});
+
+new Vue({
+  el: '#app2',
+  data: {
+    message: 'Hello there'
   }
 });
